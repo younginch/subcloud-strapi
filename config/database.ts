8070f268216111ec6a50bc60820b9e68789514f4
Seungbin Oh <sboh1214@gmail.com>
2022-08-01 20:51:54 +0900

    Change to existing pg

--- config/database.ts
+++ config/database.ts
@@ -4,14 +4,12 @@ export default ({ env }) => ({
     connection: {
       host: env(
         "DATABASE_HOST",
-        "subcloud-strapi.c66c7hkpmaeq.ap-northeast-2.rds.amazonaws.com"
+        "subcloud-prod.c66c7hkpmaeq.ap-northeast-2.rds.amazonaws.com"
       ),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", "sub"),
       password: env("DATABASE_PASSWORD", "dlalsrbditmaktmxj"),
     },
-    useNullAsDefault: true,
-    acquireConnectionTimeout: 600000,
   },
 });
