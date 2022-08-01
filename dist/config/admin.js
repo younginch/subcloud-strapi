9002a69e07877e5ecad04ec594008afc73374652
Seungbin Oh <sboh1214@gmail.com>
2022-08-01 20:03:57 +0900

    remove dist folder

--- dist/config/admin.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ({ env }) => ({
-    auth: {
-        secret: env('ADMIN_JWT_SECRET', '9cd0dc688c3391b3d55bd2fde7e377ab'),
-    },
-});
