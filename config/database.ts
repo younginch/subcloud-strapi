b9736449645a1ab47a17ec98e4de802020eda35e
Seungbin Oh <sboh1214@gmail.com>
2022-08-01 19:09:27 +0900

    Change db url

--- config/database.ts
+++ config/database.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "subcloud-prod.c66c7hkpmaeq.ap-northeast-2.rds.amazonaws.com"),
+      host: env("DATABASE_HOST", "subcloud-strapi.c66c7hkpmaeq.ap-northeast-2.rds.amazonaws.com"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", "sub"),
