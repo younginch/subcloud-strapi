a69eb6b6402e9e3a32d16a1a4e4e1dcf58e77a36
Seungbin Oh <sboh1214@gmail.com>
2022-08-05 12:58:41 +0900

    Fix env

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,12 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 443),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", [
+      "8tLADoM8+JIKrZ52CfY9gQ==",
+      "wQ+AIlrwAoBYOwdLkPYpYw==",
+      "p0yD9Ky/N515XvinxcuLug==",
+      "E4N/UJiAeZlTnLif9ImR6Q==",
+    ]),
   },
 });
