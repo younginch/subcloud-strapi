2d30acae9bc0cebab3c96a0d0f172476ab6911f9
Seungbin Oh <sboh1214@gmail.com>
2022-08-14 19:20:02 +0900

    Add first user config

---
+++ src/api/first-user-config/controllers/first-user-config.ts
@@ -0,0 +1,7 @@
+/**
+ *  first-user-config controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::first-user-config.first-user-config');
---
+++ src/api/first-user-config/routes/first-user-config.ts
@@ -0,0 +1,7 @@
+/**
+ * first-user-config router.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::first-user-config.first-user-config');
